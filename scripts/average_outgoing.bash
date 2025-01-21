5623f95418fbfd20e163f039d042385d7357c0dd
Jackson Mowry <jacksonm@duck.com>
2025-01-21 15:34:09 -0500

    remove double vector length

--- scripts/average_outgoing.bash
+++ scripts/average_outgoing.bash
@@ -7,7 +7,12 @@ main() {
     fi
     local network="${1}"
 
-    echo "Average Fan-out:" "$(jq '(.Edges | length) / (.Nodes | length)' "${network}")"
+    average=$(jq '(.Edges | length) / (.Nodes | length)' "${network}")
+    median=$(jq '(.Nodes | length) as $nlen | [.Edges[].from] | sort | group_by(.) | map(length) | .[(length / 2) - 1]' "${network}")
+    # histogram=$(jq '.Edges as $e | [.Nodes | map(.id) | map({id: ., outgoingEdges: ( . as $id | [$e[] | select(.from == $id)] | length) }) | .[].outgoingEdges]' "${network}")
+
+    echo "Average Fan-out:" "${average}"
+    echo "Median Fan-out:" "${median}"
 }
 
 main "${@}"
